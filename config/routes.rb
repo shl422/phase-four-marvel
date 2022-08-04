a0f2a54059b20dfacdd513cfebf0d40bef8b7fd6
Shell <h.k.domingo@outlook.com>
2022-08-04 20:32:18 +0800

    added ability to add todo list add todo items to lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {
-    :registrations => "registrations"
+    :registrations => "registrations",
+    :omniauth_callbacks => "callbacks"
   }
 
   devise_scope :user do 
@@ -8,5 +9,15 @@ Rails.application.routes.draw do
     get "signup", to: "devise/registrations#new"
   end
 
+  resources :todo_lists do
+    resources :todo_items do
+    member do
+    patch :complete
+     end
+    end
+   end
+  
+
+  root 'application#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_094329) do
+ActiveRecord::Schema.define(version: 2022_08_04_100846) do
+
+  create_table "todo_items", force: :cascade do |t|
+    t.string "content"
+    t.integer "todo_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,8 +37,11 @@ ActiveRecord::Schema.define(version: 2022_08_04_094329) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
